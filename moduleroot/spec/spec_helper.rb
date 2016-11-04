10abf9d392d64ceb575b71b9bd8daea0c7bebe0f
Dennis Hoppe <github@debian-solutions.de>
2016-11-04 14:39:11 +0100

    Add support for default facts

--- moduleroot/spec/spec_helper.rb
+++ moduleroot/spec/spec_helper.rb
@@ -16,6 +16,19 @@ unless RUBY_VERSION =~ %r{^1.9}
   end
 end
 
+RSpec.configure do |c|
+  default_facts = {
+    puppetversion: Puppet.version,
+    facterversion: Facter.version
+  }
+  default_facts.merge!(YAML.load(File.read(File.expand_path('../default_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_facts.yml', __FILE__))
+  default_facts.merge!(YAML.load(File.read(File.expand_path('../default_module_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_module_facts.yml', __FILE__))
+  c.default_facts = default_facts
+  <%- if @configs['mock_with'] -%>
+  c.mock_with <%= @configs['mock_with'] %>
+  <%- end -%>
+end
+
 <%- [@configs['spec_overrides']].flatten.compact.each do |line| -%>
  <%= line %>
 <%- end -%>
